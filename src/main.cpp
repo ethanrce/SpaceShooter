0ee0140faf8e5c9e9bbe5c757d3ab2123f07c4d2
ethanrce <ethanreece@berkeley.edu>
2023-05-17 01:31:51 -0700

    Got background image on screen, need to resize to full window

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
-#include "raylib.h"
-#include "raymath.h"
-
+#include <raylib.h>
+#include <raymath.h>
+#include <fstream>
 #include <iostream>
 
-int main(void) {
-    
-    InitWindow(400, 224, "Template C++");
 
-    std::cout << "Hello, World! Proof of C++" << std::endl;
+int main(void) {
+    int height = 800;
+    int width = 800;
+
+    InitWindow(height, width, "SpaceShooter");
+    // Limits FPS to refresh rate of monitor
+    SetWindowState(FLAG_VSYNC_HINT);
+
+    //std::cout << "Hello world";
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
-
+        
         ClearBackground(RAYWHITE);
+        const char *background = "assets/River/background.png";
+        Texture2D background2 = LoadTexture(background);
+        DrawTexture(background2, height/2, width/2, RAYWHITE);
+
+        LoadImageFromScreen();
+        DrawFPS(10, 10);
 
         EndDrawing();
     }
