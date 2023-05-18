0e557ca609e042292105f2d16f1f9313dc2e3cf0
ethanrce <ethanreece@berkeley.edu>
2023-05-18 15:36:36 -0700

    Window is resizable and background covers the entire screen

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,29 @@ int main(void) {
     int height = 800;
     int width = 800;
 
+    SetWindowState(FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
     InitWindow(height, width, "SpaceShooter");
     // Limits FPS to refresh rate of monitor
-    SetWindowState(FLAG_VSYNC_HINT);
+
 
     Texture2D background = LoadTexture("assets/River/background.png");
-    //std::cout << "Hello world";
+    NPatchInfo info = { (Rectangle) {0.0f, 0.0f, (float) background.width, (float) background.height}, 0, 0, 0, 0, NPATCH_NINE_PATCH};
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
 
-        DrawTextureEx(background, GetWindowPosition(), 0.0f, 2.0f, WHITE);
+        ClearBackground(RAYWHITE);
+
+        DrawTextureNPatch(background, info, (Rectangle) {0.0f, 0.0f, (float) GetScreenWidth(), (float) GetScreenHeight()}, Vector2Zero(), 0.0f, RAYWHITE);
         DrawFPS(10, 10);
 
         EndDrawing();
     }
 
-
+    UnloadTexture(background);
     CloseWindow();
 
     return 0;
