78e5dbde7465f5a27246729d0f2c755178b68f1a
ethanrce <ethanreece@berkeley.edu>
2023-05-20 22:54:49 -0700

    Got main character animation wokringg

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,40 @@ int main(void) {
     int width = 800;
 
     // Limits FPS to refresh rate of monitor
-    SetWindowState(FLAG_VSYNC_HINT);
+    //SetWindowState(FLAG_VSYNC_HINT);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
     // Rectangle = {xf, yf, widthf, heightf}
     // Vector2 (xf, yf)
 
     InitWindow(height, width, "SpaceShooter");
+    int FPS = 60;
+    SetTargetFPS(FPS);
 
     Texture2D background = LoadTexture("assets/River/background.png");
     NPatchInfo info = {(Rectangle) {0.0f, 0.0f, (float) background.width, (float) background.height}, 0, 0, 0, 0, NPATCH_NINE_PATCH};
 
+    
     Texture2D ship = LoadTexture("assets/spritesheets/ship.png");
-   
+    // Main character sprite animation settings
+    Rectangle shipRec = {0, 0, (float)((ship.width/5.0f)), (float)(ship.height/2.0f)};
     int spriteframe = 0;
+    int framespeed = 8;
+    int framecounter = 0;
 
     while (!WindowShouldClose()) {
-        // Main character sprite animation settings
-        spriteframe ++;
-        Rectangle shipRec = {0, 0, (float)((ship.width/5.0f) * spriteframe), (float)(ship.height/2.0f)};
+        // Main character sprite animation controller
+        framecounter ++;
+        if (framecounter >= (FPS/framespeed)) {
+            spriteframe ++;
+            framecounter = 0;
+
+            if (spriteframe > 5) {
+                spriteframe = 1;
+            }
+
+            shipRec.x = ((float)ship.width/5) * spriteframe;
+        }
 
         // Main character sprite scaling settings
         float scalerate = 7.0f;
