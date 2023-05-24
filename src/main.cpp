a9f26f94f3c252aa5d13a8a5c16737f73fcc58f4
ethanrce <ethanreece@berkeley.edu>
2023-05-24 10:58:47 -0700

    Added lasers dequeuing after leaving screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,23 @@
 #include <iostream>
 using namespace std;
 #include "player.h"
+#include <vector>
 
 #define FPS 60
 #define FRAMESPEED 8
-
+#define SHIPSPEED 1.5f
+#define LASERSPEED 2.0f
+#define LASERCOOLDOWN 1
 // Rectangle = {xf, yf, widthf, heightf}
 // Vector2 (xf, yf)
 
 int main(void) {
-    int height = 600;
-    int width = 600;
+    int height = 400;
+    int width = 400;
 
     // Limits FPS to refresh rate of monitor
     //SetWindowState(FLAG_VSYNC_HINT);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-
-
 
     InitWindow(height, width, "SpaceShooter");
     SetTargetFPS(FPS);
@@ -39,11 +40,19 @@ int main(void) {
     Vector2 origin = {(float)(position.width/2.0f), (float)(position.height/2.0f)};
     float rotation = 0.0f;
 
+    // Laser position/animation settings
     Texture2D laser = LoadTexture("assets/spritesheets/laser-bolts.png");
     vector<Laser> lasers;
     int lasercounter = 0;
     int laserframe = 0;
+    int laserx;
+    int lasery;
 
+    // Laser cooldown settings
+    int lasershootcounter = 0;
+    bool canShoot = true;
+
+    Texture2D enemys = LoadTexture("assets/spritesheets/enemy-small.png");
     while (!WindowShouldClose()) {
         // Main character sprite animation controller
         framecounter ++;
@@ -62,37 +71,58 @@ int main(void) {
         // TODO: Need to fix borders during window resizing in main menu.
         // TODO: Allow for rotation speed slider in main menu
         if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
-            shipRec.y = ((float)(ship.height/2.0f));
-            position.x += 2.0f;
+            if (position.x < (GetScreenWidth() - (position.width/2.0f))) {
+            shipRec.y = (ship.height/2.0f);
+            position.x += SHIPSPEED;
             rotation = 90.0f;
-            // rotation += 5.0f;
+            }
         }
         if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
-            shipRec.y = ((float)(ship.height/2.0f));
-            position.x -= 2.0f;
+            if (position.x > (position.width/2.0f)) {
+            shipRec.y = (ship.height/2.0f);
+            position.x -= SHIPSPEED;
             rotation = 270.0f;
-            // rotation -= 5.0f;
+            }
         }
-
         if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-         //   if ((float)(position.y) < ((float)((GetScreenHeight()/2.0f)))) {
-                position.y -= 2.0f;
+            if (position.y > (position.height/2.0f)) {
+                position.y -= SHIPSPEED;
                 rotation = 0.0f;
-                shipRec.y = ((float)(ship.height/2.0f));
-         //   }
+                shipRec.y = ship.height/2.0f;
+            }
+        }
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)){
+            if (position.y < (GetScreenHeight() - (position.height/2.0f))) {
+                position.y += SHIPSPEED;
+                rotation = 180.0f;
+                shipRec.y = (ship.height/2.0f);
+            }
         }
 
-        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)){
-         //  if ((float)(position.y - scale.height) > ((float)(-1*(GetScreenHeight()/2.0f)))) {
-                position.y += 2.0f;
-                rotation = 180.0f;
-                shipRec.y = ((float)(ship.height/2.0f));
-         //   }
+        // Controls laser shooting cooldown
+        lasershootcounter ++;
+        if (lasershootcounter >= (FPS/LASERCOOLDOWN) && !canShoot) {
+            lasershootcounter = 0;
+            canShoot = true;
         }
-        
-        // TODO: Need to add cooldown before being able to shoot again
-        if (IsKeyDown(KEY_F)) {
-           Laser newLaser = shootLaser(position.x, position.y - (position.height/2.0f), rotation, laser.width, laser.height, scalerate);
+
+        // Controls initiating shooting & initial position of laser
+        if (IsKeyPressed(KEY_F) && canShoot) {
+            canShoot = false;
+            if (rotation == 0.0f) {
+                laserx = position.x;
+                lasery = position.y - (position.height/2.0f);
+            } else if (rotation == 90.0f) {
+                laserx = position.x + (position.width/2.0f);
+                lasery = position.y;
+            } else if (rotation == 180.0f) {
+                laserx = position.x;
+                lasery = position.y + (position.height/2.0f);
+            } else {
+                laserx = position.x - (position.width/2.0f);
+                lasery = position.y;
+            }
+           Laser newLaser = shootLaser(laserx, lasery, rotation, laser.width, laser.height, scalerate/2.0f);
            lasers.push_back(newLaser);
         }
 
@@ -117,10 +147,23 @@ int main(void) {
             }
             for (int i = 0; i < lasers.size(); i++) {
                 lasers[i].laserRec.x = ((float)(laser.width/2.0f) * laserframe);
-                lasers[i].position.y -= 1.5f;
+                if (lasers[i].rotation == 0.0f) {
+                    lasers[i].position.y -= LASERSPEED;
+                } else if (lasers[i].rotation == 90.0f) {
+                    lasers[i].position.x += LASERSPEED;
+                } else if (lasers[i].rotation == 180.0f) {
+                    lasers[i].position.y += LASERSPEED;
+                } else {
+                    lasers[i].position.x -= LASERSPEED;
+                }
                 DrawTexturePro(laser, lasers[i].laserRec, lasers[i].position, lasers[i].origin, lasers[i].rotation, RAYWHITE);
+                // Dequeue's laser after it's off screen
+                if (lasers[i].position.x > GetScreenWidth() || lasers[i].position.x < 0 || lasers[i].position.y > GetScreenHeight() || lasers[i].position.y < 0) {
+                    lasers.erase(lasers.begin() + i - 1);
+                }
             }
         }
+        cout << lasers.size() << endl;
 
         DrawFPS(10, 10);
 
@@ -130,6 +173,7 @@ int main(void) {
     UnloadTexture(background);
     UnloadTexture(ship);
     UnloadTexture(laser);
+    UnloadTexture(enemys);
     CloseWindow();
 
     return 0;
